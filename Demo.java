58cd38b22c4a91bfd55b15a3844869f8b97f5af0
Nagbhushan244 <01fe23bca244@kletech.ac.in>
2025-04-23 15:06:07 +0530

    save with changes

--- Demo.java
+++ Demo.java
@@ -1,14 +1,18 @@
  class Demojava{
-    int a,b,res1,res2;
+    int a,b,res1,res2,res3,res4;
     public Demojava(int a,int b)
     {
         this.a=a;
         this.b=b;
         res1=a+b;
         res2=a-b;
+        res3=a*b;
+        res4=a/b;
 
         System.out.println("Addition Result:"+res1);
         System.out.println("Subtraction Result:"+res2);
+        System.out.println("Multiplication Result:"+res3);
+        System.out.println("Division Result:"+res4);
 
     }
 }
